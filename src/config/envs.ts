19697c7ceda90e79ebfcf85c3c9c4db5a18fbaec
Leandro Arturi <lea.arturi@gmail.com>
2024-04-28 18:01:48 -0300

    Initial commit

---
+++ src/config/envs.ts
@@ -0,0 +1,30 @@
+import { Logger } from '@nestjs/common';
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate({
+  ...process.env,
+});
+
+const logger = new Logger('Main');
+
+if (error) {
+  logger.error(`Config validation error: ${error.message}`);
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+};
---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-payment-session')
+  createPaymentSession() {
+    return {
+      createPaymentSession: 'createPaymentSession',
+    };
+  }
+
+  @Get('success')
+  success() {
+    return {
+      ok: true,
+      message: 'Payment created successfully',
+    };
+  }
+
+  @Get('cancel')
+  cancel() {
+    return {
+      ok: false,
+      message: 'Payment cancelled',
+    };
+  }
+
+  @Post('webhook')
+  async stripeWebHook() {
+    return {
+      stripeWebHook: 'stripeWebHook',
+    };
+  }
+}
