118c6df9e1eaf47fdd55e7e65affffafcc5d416c
Leandro Arturi <lea.arturi@gmail.com>
2024-05-01 13:38:42 -0300

    Microservicio hibrido - REST / NATS

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Payments Microservice');
@@ -17,6 +18,18 @@ async function bootstrap() {
     }),
   );
 
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+    { inheritAppConfig: true },
+  );
+
+  // App hibrida, esta abierta en el puerto 3003 y asimismo integrada a NATS
+  await app.startAllMicroservices();
   await app.listen(envs.port);
 
   logger.log(`Payments Microservice runing on port ${envs.port}`);
